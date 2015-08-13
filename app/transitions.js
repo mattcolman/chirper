0f7a33ec86b1b608398f4839069f87060e122256
Matt Colman <matt.colman@blake.com.au>
2015-08-13 14:07:31 +1000

    liquid fire

---
+++ app/transitions.js
@@ -0,0 +1,18 @@
+// app/transitions.js
+
+export default function(){
+  this.transition(
+    this.fromRoute('user.index'),
+    this.toRoute('user.following'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.fromRoute('home'),
+    this.toRoute('user'),
+    this.useAndReverse('explode', {
+      matchBy: 'data-profile-image',
+      use: ['toUp', {duration: 1000}]
+    })
+  );
+}
