b2d8c604c4108302222b6863859fd28451d0b226
Matt Colman <matt.colman@blake.com.au>
2015-08-14 13:33:34 +1000

    consuming the REST API

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,8 @@
 // app/application/adapter.js
 
+import config from '../config/environment';
 import DS from 'ember-data';
 
-export default DS.FixtureAdapter;
+export default DS.RESTAdapter.reopen({
+  host: config.apiURL
+});
--- config/environment.js
+++ config/environment.js
@@ -6,11 +6,16 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    apiURL: 'http://localhost:9000',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       }
+    },
+
+    contentSecurityPolicy: {
+      'connect-src': "http://localhost:9000"
     },
 
     APP: {
