bc147b27a9e482d86de582b951d28b7a13ed2f6f
Matt Colman <matt.colman@blake.com.au>
2015-08-18 20:57:08 +1000

    trying to filter chirps on the home page to show only chirps from current user and followees. this sort of works, with a few bugs…

--- app/home/route.js
+++ app/home/route.js
@@ -4,9 +4,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    //console.log('hi', this.get('session.currentUser.id'));
     return Ember.RSVP.hash({
-      chirps: this.store.query('user', { me: true }).then((users) => {
-        return users.get('firstObject').get('chirps')
+      chirps: this.store.query('user', {me: true}).then((users)=> {
+        var currentUser = users.get('firstObject')
+        var followers = currentUser.get('followees')
+        var chirpies = currentUser.get('chirps')
+        followers.forEach((user) => {
+          chirpies.pushObjects(user.get('chirps'))
+        })
+        return chirpies;
       })
     });
   },
